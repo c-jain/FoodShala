4facbf6c6e39b2af23cc96867bd915990f663a5c
Lalit-N97 <lalitnegi1997@gmail.com>
2020-06-24 20:47:00 +0530

    nav fix

--- utility/DB.js
+++ utility/DB.js
@@ -11,7 +11,7 @@
 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('FoodShalaDB', 'root', '', {
+const sequelize = new Sequelize('FoodShalaDB', 'root', 'Lalit1997@', {
     dialect: 'mysql',
     host: 'localhost'
 });
